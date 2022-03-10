4419ac742ed04eefa74bf4329fa743e6b2ad68a3
Deniz <denizoguz@gmail.com>
2022-03-10 14:48:09 +0300

    added similar issues tab extension

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -34,11 +34,11 @@ def app_specific_action(webdriver, datasets):
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:view_issue_with_similar_issues_tab")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}?page=com.deniz.jira.similarissues:similar-issues-tab-panel")
             page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.ID, "similar_issue_overlay_table"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
 
