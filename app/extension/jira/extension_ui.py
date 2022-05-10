b748c8875c9f28a348bd37aff9c1e466cc3bd739
Deniz <denizoguz@gmail.com>
2022-05-10 14:42:16 +0300

    added view worklog tab and view weekly timesheet app specific actions

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -8,37 +8,36 @@ from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def view_issue_with_similar_issues(webdriver, datasets):
     page = BasePage(webdriver)
+    # Add custom_dataset_query: worklogAuthor is not EMPTY to jira.yml file
     if datasets['custom_issues']:
         issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    @print_timing("selenium_app_custom_action")
+    def measure():
+        @print_timing("selenium_app_custom_action:view_issue_with_wp_worklog_tab")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}?page=com.deniz.jira.worklog:com.deniz.jira.worklog.worklog-issue-tab-panel")
+            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
+            page.wait_until_visible((By.ID, "worklog-by-user-table"))  # Wait for summary field visible
+            page.wait_until_visible((By.ID, "wp-ts-worklog-detail-popup"))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+    measure()
+
+def view_issue_with_similar_issues(webdriver, datasets):
+    page = BasePage(webdriver)
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        @print_timing("selenium_app_custom_action:view_weekly_project_timesheet")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/WPShowTimesheetAction!customTimesheet.jspa?periodMode=WEEK&targetType=USER&calendarType=CUSTOM&groupingType=ISSUE"
+                           f"#targetType=USER&targetKey=currentUser()&groupingType=Project,"
+                           f"Issue&periodMode=PERIOD&startDate=2020-09-02&endDate=2020-09-08&&&periodLocked=false&calendarType=CUSTOM&saveToUserHistory=false&extraIssueFilter=&showIssuesWithoutWorklog=false&viewType=TIMESHEET")
             page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.ID, "worklog-by-user-table"))  # Wait for summary field visible
+            page.wait_until_visible((By.CSS_SELECTOR, "p.wp-weekly-total"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
 
